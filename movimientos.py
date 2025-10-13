2cf2d64638ea627ab175dc1b6f12bd7cf2eaed0b
Esteban <esteban.racana@gmail.com>
2025-10-13 17:39:51 -0300

    Actualizacion
    Se elimino lo que se necesita para que funcione para el proyecto

--- movimientos.py
+++ movimientos.py
@@ -4,7 +4,7 @@ class movimientos:
     def __init__(self):
         self.x=0
         self.y=0
-        self.velocidad=1
+        self.velocidad=velocidad
         self.moviendo_arriba=False
         self.movimiento_abajo=False
         self.movimiento_izquierda=False
@@ -21,31 +21,4 @@ class movimientos:
     def posicion(self):
         return(self.x,self.y)
 
-movimientos=movimientos()
-jugando=True
-while jugando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            jugando=False
-            pygame.quit()
-            quit()
-        if evento.type==pygame.KEYDOWN:
-            if evento.key==pygame.K_w:
-                movimientos.moviendo_arriba=True
-            elif evento.key==pygame.K_s:
-                movimientos.moviendo_abajo=True
-            elif evento.key==pygame.K_d:
-                movimientos.moviendo_izquierda=True
-            elif evento.key==pygame.K_a:
-                movimientos.moviendo_derecha=True
-        elif evento.type==pygame.KEYUP:
-            if evento.key==pygame.K_w:
-                movimientos.moviendo_arriba=False
-            elif evento.key==pygame.K_s:
-                movimientos.moviendo_abajo=False
-            elif evento.key==pygame.K_d:
-                movimientos.moviendo_derecha=False
-            elif evento.key==pygame.K_a:
-                movimientos.moviendo_izquierda=False
-movimientos.actualizar()
-print(f"Posición: {movimientos.posicion()}")
+
